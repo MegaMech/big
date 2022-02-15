eed91bbd4e1aa873d7b6b3a13028079bb87f6049
MegaMech <inkstudios1@hotmail.com>
2022-02-14 18:31:37 -0700

    update

--- main.js
+++ main.js
@@ -10,20 +10,19 @@ window.onload = function()
     });
      setTimeout(function(){
          document.getElementById("loadspin").style.opacity = 0;
-         document.getElementById("logoImg").style.width = "450px";
-         document.getElementById("logoImg").style.marginTop = "2%";
+         //document.getElementById("logoImg").style.width = "450px";
+         //document.getElementById("logoImg").style.marginTop = "2%";
          setTimeout(function(){
              document.getElementsByTagName("body")[0].style.overflow = "auto";
              document.getElementsByTagName("body")[0].style.position = "relative";
-             document.getElementsByTagName("nav")[0].style.zIndex = "15";
-             document.getElementsByTagName("nav")[0].style.opacity = "1";
+             //document.getElementsByTagName("nav")[0].style.zIndex = "15";
+             //document.getElementsByTagName("nav")[0].style.opacity = "1";
              document.getElementById("contentDisplay").style.zIndex = "1";
              document.getElementById("contentDisplay").style.opacity = "1";
              document.getElementsByTagName("footer")[0].style.zIndex = "1";
              document.getElementsByTagName("footer")[0].style.opacity = "1";
              document.getElementById("loadspin").remove();
              position = Object.keys(comics).length - 1;
-             comic(0);
          }, 1000);
 
      }, 1000);
@@ -40,6 +39,20 @@ function menu()
     }
     return true;
 }
+
+function displayComic() {
+    minimizeNav();
+    comic(0);
+}
+
+function minimizeNav() {
+    document.getElementsByTagName("nav")[0].style.display = "none";
+}
+
+function maximizeNav() {
+    document.getElementsByTagName("nav")[0].style.display = "block";
+}
+
 function comic(direction)
 {
     position = position + direction;
@@ -49,8 +62,8 @@ function comic(direction)
     var urlString = "url('comics/"+comics[position].FileName+"')";
     console.log(urlString);
     document.getElementsByClassName("comic")[0].style.backgroundImage = urlString;
-    document.getElementById("comicTitle").innerHTML = comics[position].Title;
-    document.getElementById("comicDate").innerHTML = comics[position].Date;
+    //document.getElementById("comicTitle").innerHTML = comics[position].Title;
+    //document.getElementById("comicDate").innerHTML = comics[position].Date;
 }
 function footerLink()
 {
